d45303bda24f78bae3efad395c8fac917ca02885
illkkeerr <ilkersenel5797@gmail.com>
2023-07-03 20:11:10 +0300

    Asp.Net_Mvc_Kisisel_Website_Yapimi

---
+++ KisiselWebSitesi/KisiselWebSitesi/Models/Siniflar/Admin.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace KisiselWebSitesi.Models.Siniflar
+{
+    public class Admin
+    {
+        [Key]
+        public int Id { get; set; }
+        public string KullaniciAdi { get; set; }
+        public string Sifre { get; set; }
+    }
+}
